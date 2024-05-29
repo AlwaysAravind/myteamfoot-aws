a94065aad77556088e7d5441a47b6ed456bd7d3c
AlwaysAravind <rolexaravind@gmail.com>
2024-05-29 18:30:22 +0530

    Hello World

--- selenium_imports.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-import time
-import requests
