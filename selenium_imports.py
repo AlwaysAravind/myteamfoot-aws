a358f937fc4eecd867c670b5f7a806c04dba204e
AlwaysAravind <rolexaravind@gmail.com>
2024-05-29 12:12:26 +0530

    AWS Learning

---
+++ selenium_imports.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+import time
+import requests
